a766199b281054bcfecd51d4dae7341f5660c7cf
Ahmmmyoo <ahmadirfankhan@gmail.com>
2024-07-31 21:15:09 +0500

    data structure

---
+++ dataStr.js
@@ -0,0 +1,29 @@
+// ARRAY
+
+var list = ["tiger", "cat", "bear", "bird"];
+
+console.log(list);
+
+console.log(list[0]);
+
+var numbers = [0, 1, 2, 3, 4, 5, 6,];
+
+var booleans = [true, false, true, true, false];
+
+var funcList = [function apple() {
+  console.log("apple");
+}];
+
+var mixedList = ["pear", 3, undefined, true, function cars() { console.log("Suzuki"); }];
+
+// OBJECT
+
+var user = {
+  name: "Jhon",
+  age: 45,
+  hobby: "Soccer",
+  isMarried: false,
+};
+
+user.favoriteFood = "banana";
+
