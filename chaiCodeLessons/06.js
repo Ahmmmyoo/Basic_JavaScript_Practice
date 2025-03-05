417cbaddc8d105bc835c670b66b47a698706a88f
Ahmmmyoo <ahmadirfankhan@gmail.com>
2025-03-05 10:59:15 +0500

    datatypes summary

---
+++ chaiCodeLessons/06.js
@@ -0,0 +1,18 @@
+// Datatypes of JavaScript Summary 
+
+// Primitive 
+
+// 7 Types: 
+let str = String;       // String
+let num = Number;       // Number
+let bool = Boolean;     // Boolean
+let n = null;           // null
+let undef = undefined;  // undefined
+let syb = Symbol;       // Symbol
+let bInt = BigInt;      // BigInt
+
+// Reference Type (Non Primitive)
+
+// Array
+// Object
+// Function
