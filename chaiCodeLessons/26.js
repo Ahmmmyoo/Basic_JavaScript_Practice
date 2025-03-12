338598c34af5e27a025f9826ea5f66c553fd3185
Ahmmmyoo <ahmadirfankhan@gmail.com>
2025-03-12 06:44:38 +0500

    DOM introduction

---
+++ chaiCodeLessons/26.js
@@ -0,0 +1,33 @@
+// DOM introduction in JS
+
+// console.log(window);
+// console.log(document);
+// console.dir(document);
+
+
+// const obj = {
+//   name: "K",
+//   age: 20,
+//   city: "New York",
+//   printName: function() {
+//     console.log(this.name);
+//   },
+//   printAge: function() {
+//     console.log(this.age);
+//   },
+//   printCity: function() {
+//     console.log(this.city);
+//   },
+//   getName: function() {
+//     return this.name
+//   },
+//   getAge: function() {
+//     return this.age
+//   },
+//   getCity: function() {
+//     return this.city
+//   }
+// }
+// console.log(obj);
+// console.dir(obj);
+
