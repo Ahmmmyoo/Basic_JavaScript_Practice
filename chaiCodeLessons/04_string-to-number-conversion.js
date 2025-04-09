326e154fa84e9eddb6b1b41085049faae9b23515
Ahmmmyoo <ahmadirfankhan@gmail.com>
2025-04-09 20:23:01 +0500

    Refactor string to number conversion examples for clarity and consistency

--- chaiCodeLessons/04_string-to-number-conversion.js
+++ chaiCodeLessons/04_string-to-number-conversion.js
@@ -11,17 +11,16 @@ let str1 = "hello";
 let str2 = " ryu";
 // console.log(str1 + str2); // hello ryu
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 3);
-// console.log(1 + 2 + "3");
-// console.log(1 + 5 * 3 / 2);
+console.log(typeof ("1" + 2), ("1" + 2));  // string 12
+console.log(typeof (1 + "2"), (1 + "2")); // string 12
+console.log(typeof ("1" + 2 + 3), ("1" + 2 + 3)); // string 123
+console.log(typeof (1 + 2 + "3"), (1 + 2 + "3")); // string 33
+console.log(typeof (1 + 5 * 3 / 2), (1 + 5 * 3 / 2)); // number
 
-// console.log(+true);
-// console.log(typeof +true);
-// console.log(+"");
-
-// let abc = 4;
-// console.log(abc++);
-// console.log(++abc);
+console.log(typeof (+true), (+true)); // number 1
+console.log(typeof (typeof +true), (typeof +true)); // string number
+console.log(typeof (+""), (+"")); // number 0
+let abc = 4;
+console.log(abc, (abc++), (abc)); // 4 4 5
+console.log(abc, (++abc), (abc)); // 5 6 6
 
