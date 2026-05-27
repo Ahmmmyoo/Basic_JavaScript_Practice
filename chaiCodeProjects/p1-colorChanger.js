27b861dfc73960824e37728d8c8916aa085031ed
Ahmmmyoo <ahmadirfankhan@gmail.com>
2026-05-27 19:11:30 +0500

    added Project 1

---
+++ chaiCodeProjects/p1-colorChanger.js
@@ -0,0 +1,205 @@
+const body = document.querySelector('body');
+
+function getBox() {
+  return document.querySelectorAll('.box')
+}
+let box = getBox();
+
+function randomHexColor() {
+  return (
+    '#' +
+    Math.floor(Math.random() * 0xffffff)
+      .toString(16)
+      .padStart(6, '0')
+  );
+}
+
+function makeSpan() {
+  const span = document.createElement('span');
+  const rHC = randomHexColor();
+  span.setAttribute('id', rHC);
+  span.setAttribute('class', 'box');
+  span.appendChild(document.createTextNode(rHC));
+  span.style.backgroundColor = rHC;
+  span.style.margin = '5px';
+  document.querySelector('.box').append(span);
+  // console.log(span);
+}
+
+const limit = 20;
+if (0)
+  for (let i = 0; i <= limit; i++) {
+    makeSpan();
+  }
+
+const htmlColors = [
+  "AliceBlue",
+  "AntiqueWhite",
+  "Aqua",
+  "Aquamarine",
+  "Azure",
+  "Beige",
+  "Bisque",
+  "Black",
+  "BlanchedAlmond",
+  "Blue",
+  "BlueViolet",
+  "Brown",
+  "BurlyWood",
+  "CadetBlue",
+  "Chartreuse",
+  "Chocolate",
+  "Coral",
+  "CornflowerBlue",
+  "Cornsilk",
+  "Crimson",
+  "Cyan",
+  "DarkBlue",
+  "DarkCyan",
+  "DarkGoldenRod",
+  "DarkGray",
+  "DarkGreen",
+  "DarkKhaki",
+  "DarkMagenta",
+  "DarkOliveGreen",
+  "DarkOrange",
+  "DarkOrchid",
+  "DarkRed",
+  "DarkSalmon",
+  "DarkSeaGreen",
+  "DarkSlateBlue",
+  "DarkSlateGray",
+  "DarkTurquoise",
+  "DarkViolet",
+  "DeepPink",
+  "DeepSkyBlue",
+  "DimGray",
+  "DodgerBlue",
+  "FireBrick",
+  "FloralWhite",
+  "ForestGreen",
+  "Fuchsia",
+  "Gainsboro",
+  "GhostWhite",
+  "Gold",
+  "GoldenRod",
+  "Gray",
+  "Green",
+  "GreenYellow",
+  "HoneyDew",
+  "HotPink",
+  "IndianRed",
+  "Indigo",
+  "Ivory",
+  "Khaki",
+  "Lavender",
+  "LavenderBlush",
+  "LawnGreen",
+  "LemonChiffon",
+  "LightBlue",
+  "LightCoral",
+  "LightCyan",
+  "LightGoldenRodYellow",
+  "LightGray",
+  "LightGreen",
+  "LightPink",
+  "LightSalmon",
+  "LightSeaGreen",
+  "LightSkyBlue",
+  "LightSlateGray",
+  "LightSteelBlue",
+  "LightYellow",
+  "Lime",
+  "LimeGreen",
+  "Linen",
+  "Magenta",
+  "Maroon",
+  "MediumAquaMarine",
+  "MediumBlue",
+  "MediumOrchid",
+  "MediumPurple",
+  "MediumSeaGreen",
+  "MediumSlateBlue",
+  "MediumSpringGreen",
+  "MediumTurquoise",
+  "MediumVioletRed",
+  "MidnightBlue",
+  "MintCream",
+  "MistyRose",
+  "Moccasin",
+  "NavajoWhite",
+  "Navy",
+  "OldLace",
+  "Olive",
+  "OliveDrab",
+  "Orange",
+  "OrangeRed",
+  "Orchid",
+  "PaleGoldenRod",
+  "PaleGreen",
+  "PaleTurquoise",
+  "PaleVioletRed",
+  "PapayaWhip",
+  "PeachPuff",
+  "Peru",
+  "Pink",
+  "Plum",
+  "PowderBlue",
+  "Purple",
+  "RebeccaPurple",
+  "Red",
+  "RosyBrown",
+  "RoyalBlue",
+  "SaddleBrown",
+  "Salmon",
+  "SandyBrown",
+  "SeaGreen",
+  "SeaShell",
+  "Sienna",
+  "Silver",
+  "SkyBlue",
+  "SlateBlue",
+  "SlateGray",
+  "Snow",
+  "SpringGreen",
+  "SteelBlue",
+  "Tan",
+  "Teal",
+  "Thistle",
+  "Tomato",
+  "Turquoise",
+  "Violet",
+  "Wheat",
+  "White",
+  "WhiteSmoke",
+  "Yellow",
+  "YellowGreen"
+]
+
+function makeBoxes(color) {
+  const box = document.createElement('div')
+  box.setAttribute('class', 'box')
+  box.appendChild(document.createTextNode(`${color}`))
+  document.querySelector('.div').appendChild(box)
+}
+
+makeBoxes('brown')
+htmlColors.forEach(makeBoxes)
+styleBox()
+
+function styleBox() {
+  let box = getBox()
+  box.forEach(function (box) {
+    console.log(box.innerHTML);
+    box.style.backgroundColor = box.innerHTML;
+  });
+}
+
+document.querySelectorAll('.box').forEach(function (box) {
+  // console.log(button);
+  box.addEventListener('click', function (e) {
+    // console.log(e);
+    // console.log(e.target);
+    body.style.backgroundColor = e.target.innerHTML;
+  });
+});
